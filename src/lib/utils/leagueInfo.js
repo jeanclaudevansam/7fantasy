8c65485d019fa5e62496d1d1d42fe1fb3c044cc6
jeanclaudevansam <samjoachim7@gmail.com>
2026-05-24 21:51:31 -0400

    Update leagueInfo.js
    updated dues and corrected league type

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,8 +1,8 @@
 /*   STEP 1   */
 export const leagueID = "1364427682489589760"; // your league ID
 export const leagueName = "FANTASTIC7"; // your league name
-export const dues = 100; // (optional) used in template constitution page
-export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
+export const dues = 20; // (optional) used in template constitution page
+export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
